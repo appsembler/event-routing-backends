f4d625bb7add5d824b6d4113d21aa359d2b54fc6
Tim McCormack <tmccormack@edx.org>
2021-01-07 20:34:10 +0000

    Add settings to safeguard against accidental enabling of Caliper/xAPI (#36) (Modified by Appsembler to use Django settings, not edx_toggles)
    ARCHBOM-1658
    
    Breaking change -- Caliper and xAPI processors will reject events
    unless the `CALIPER_EVENTS_ENABLED` and
    `XAPI_EVENTS_ENABLED` Django settings are enabled, respectively.

---
+++ event_routing_backends/processors/caliper/__init__.py
@@ -0,0 +1,24 @@
+"""
+Caliper processors and spec implementation.
+"""
+
+
+# APPSEMBLER Juniper
+# from edx_toggles.toggles import SettingToggle
+from django.conf import settings
+
+
+# .. toggle_name: CALIPER_EVENTS_ENABLED
+# .. toggle_implementation: SettingToggle
+# .. toggle_default: False
+# .. toggle_description: Allow sending events to external servers via Caliper.
+# .. toggle_warnings: Do not enable sending of Caliper events until there has
+#   been a thorough review of PII implications and safeguards put in place to
+#   prevent accidental leakage of novel event fields to third parties. See
+#   ARCHBOM-1655 for details.
+# .. toggle_use_cases: temporary
+# .. toggle_creation_date: 2020-01-01
+# .. toggle_target_removal_date: 2021-07-01
+# .. toggle_tickets: https://openedx.atlassian.net/browse/ARCHBOM-1658
+#CALIPER_EVENTS_ENABLED = SettingToggle("CALIPER_EVENTS_ENABLED", default=False)
+CALIPER_EVENTS_ENABLED = getattr(settings, "CALIPER_EVENTS_ENABLED", False)
